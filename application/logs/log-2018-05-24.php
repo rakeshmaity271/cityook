ea13104c7aa3d2e19e900aca9eeb9f67d10697d1
ajnishkumar <ajnishthe5@gmail.com>
2018-05-23 21:46:57 -1200

    delete empty register from employee

---
+++ application/logs/log-2018-05-24.php
@@ -0,0 +1,54 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2018-05-24 09:36:16 --> Config Class Initialized
+INFO - 2018-05-24 09:36:16 --> Hooks Class Initialized
+DEBUG - 2018-05-24 09:36:16 --> UTF-8 Support Enabled
+INFO - 2018-05-24 09:36:16 --> Utf8 Class Initialized
+INFO - 2018-05-24 09:36:16 --> URI Class Initialized
+DEBUG - 2018-05-24 09:36:16 --> No URI present. Default controller set.
+INFO - 2018-05-24 09:36:16 --> Router Class Initialized
+INFO - 2018-05-24 09:36:16 --> Output Class Initialized
+INFO - 2018-05-24 09:36:16 --> Security Class Initialized
+DEBUG - 2018-05-24 09:36:17 --> Global POST, GET and COOKIE data sanitized
+INFO - 2018-05-24 09:36:17 --> Input Class Initialized
+INFO - 2018-05-24 09:36:17 --> Language Class Initialized
+INFO - 2018-05-24 09:36:17 --> Language Class Initialized
+INFO - 2018-05-24 09:36:17 --> Config Class Initialized
+INFO - 2018-05-24 09:36:17 --> Loader Class Initialized
+INFO - 2018-05-24 09:36:17 --> Helper loaded: url_helper
+INFO - 2018-05-24 09:36:17 --> Helper loaded: html_helper
+INFO - 2018-05-24 09:36:18 --> Session: Class initialized using 'files' driver.
+INFO - 2018-05-24 09:36:18 --> Controller Class Initialized
+DEBUG - 2018-05-24 09:36:18 --> Site MX_Controller Initialized
+DEBUG - 2018-05-24 09:36:18 --> File loaded: D:\wamp\www\cityook\application\controllers/../modules/layouts/controllers/site-layout/Head.php
+DEBUG - 2018-05-24 09:36:18 --> Head MX_Controller Initialized
+DEBUG - 2018-05-24 09:36:18 --> File loaded: D:\wamp\www\cityook\application\modules/layouts/views/site-layout/head.php
+DEBUG - 2018-05-24 09:36:18 --> File loaded: D:\wamp\www\cityook\application\controllers/../modules/layouts/controllers/site-layout/Header.php
+DEBUG - 2018-05-24 09:36:18 --> Header MX_Controller Initialized
+ERROR - 2018-05-24 09:36:19 --> Severity: Compile Error --> Can't use method return value in write context D:\wamp\www\cityook\application\modules\layouts\views\site-layout\header.php 57
+INFO - 2018-05-24 09:39:56 --> Config Class Initialized
+INFO - 2018-05-24 09:39:57 --> Hooks Class Initialized
+DEBUG - 2018-05-24 09:39:57 --> UTF-8 Support Enabled
+INFO - 2018-05-24 09:39:57 --> Utf8 Class Initialized
+INFO - 2018-05-24 09:39:57 --> URI Class Initialized
+DEBUG - 2018-05-24 09:39:57 --> No URI present. Default controller set.
+INFO - 2018-05-24 09:39:57 --> Router Class Initialized
+INFO - 2018-05-24 09:39:57 --> Output Class Initialized
+INFO - 2018-05-24 09:39:57 --> Security Class Initialized
+DEBUG - 2018-05-24 09:39:57 --> Global POST, GET and COOKIE data sanitized
+INFO - 2018-05-24 09:39:57 --> Input Class Initialized
+INFO - 2018-05-24 09:39:57 --> Language Class Initialized
+INFO - 2018-05-24 09:39:57 --> Language Class Initialized
+INFO - 2018-05-24 09:39:57 --> Config Class Initialized
+INFO - 2018-05-24 09:39:57 --> Loader Class Initialized
+INFO - 2018-05-24 09:39:57 --> Helper loaded: url_helper
+INFO - 2018-05-24 09:39:57 --> Helper loaded: html_helper
+INFO - 2018-05-24 09:39:57 --> Session: Class initialized using 'files' driver.
+INFO - 2018-05-24 09:39:57 --> Controller Class Initialized
+DEBUG - 2018-05-24 09:39:57 --> Site MX_Controller Initialized
+DEBUG - 2018-05-24 09:39:57 --> File loaded: D:\wamp\www\cityook\application\controllers/../modules/layouts/controllers/site-layout/Head.php
+DEBUG - 2018-05-24 09:39:57 --> Head MX_Controller Initialized
+DEBUG - 2018-05-24 09:39:57 --> File loaded: D:\wamp\www\cityook\application\modules/layouts/views/site-layout/head.php
+DEBUG - 2018-05-24 09:39:57 --> File loaded: D:\wamp\www\cityook\application\controllers/../modules/layouts/controllers/site-layout/Header.php
+DEBUG - 2018-05-24 09:39:57 --> Header MX_Controller Initialized
+ERROR - 2018-05-24 09:39:57 --> Severity: Compile Error --> Can't use method return value in write context D:\wamp\www\cityook\application\modules\layouts\views\site-layout\header.php 57
